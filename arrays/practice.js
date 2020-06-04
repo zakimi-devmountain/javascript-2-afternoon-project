dc45462997cb2666a1f72806c2ae33658464cf4f
azakimi123 <aaron.zakimi@gmail.com>
2020-06-04 13:31:34 -0600

    problem 4

--- arrays/practice.js
+++ arrays/practice.js
@@ -35,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length - 1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -50,7 +52,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for(let i = 0; i <= family.length -1; i++){
+  alert(family[i]);}
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -65,7 +70,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for (let i = letters.length -1; i >= 0; i--){
+    alert(letters[i]);
+  }
 
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -80,7 +90,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums){
+  let evenNums = [];
+  for(let i = 0; i <= nums.length-1; i++){
+    if(i % 2 === 0){
+      evenNums += i;
+    }
+  }
 
+}
 
 
 
